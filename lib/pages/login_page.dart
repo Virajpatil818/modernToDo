b414e7660bf40b4c2a5fd02ed9d370b965e424a1
Arnav-Sangle <arnavsangle7x24@gmail.com>
2024-04-11 00:43:49 +0530

    Arnav Updated

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../components/my_button.dart';
 import '../components/my_textfield.dart';
 import '../components/square_tile.dart';
+import 'home_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-  //text editing controller
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-  void signUserIn(){}
+class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  LoginPage({super.key});
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> signIn() async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: usernameController.text,
+        password: passwordController.text,
+      );
+
+      // If successful, navigate to the next screen
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } catch (e) {
+      // Handle errors
+      print("Error signing in: $e");
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -40,48 +64,51 @@ class LoginPage extends StatelessWidget {
                     fontSize: 16,
                   ),
                 ),
-        
+
                 // Username
                 const SizedBox(height: 25),
-        
+
                 MyTextField(
                   controller: usernameController,
                   hintText: 'Username',
                   obscureText: false,
                 ),
-        
+
                 // Password
                 const SizedBox(height: 10),
-        
+
                 MyTextField(
                   controller: passwordController,
                   hintText: 'Password',
                   obscureText: true,
                 ),
-        
+
                 // Forgot password
                 SizedBox(height: 10,),
-        
+
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       Text(
-                        'Forgot password?' ,style: TextStyle(
+                          'Forgot password?' ,style: TextStyle(
                         color: Colors.grey[600],
                       )
                       ),
                     ],
                   ),
                 ),
-        
+
                 // Log in button
                 const SizedBox(height: 25,),
-                const MyButton(),
-        
+                ElevatedButton(
+                  onPressed: signIn,
+                  child: Text('Log In'),
+                ),
+
                 //Or text
-        
+
                 const SizedBox(height: 50),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25),
@@ -96,7 +123,7 @@ class LoginPage extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 10),
                         child: Text('Or continue with',style: TextStyle(
-                          color: Colors.grey[700]
+                            color: Colors.grey[700]
                         ),
                         ),
                       ),
@@ -109,7 +136,7 @@ class LoginPage extends StatelessWidget {
                     ],
                   ),
                 ),
-        
+
                 // Sign in with google
                 const SizedBox(height: 20),
                 const Row(
@@ -126,20 +153,20 @@ class LoginPage extends StatelessWidget {
                   child: Center(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                     children: [
-                       Text(
-                           'Not a member?',style: TextStyle(
-                         color: Colors.grey[700]
-                       ),
-                       ),
-                       const SizedBox(width: 10,),
-                       Text(
-                           'Register now',style: TextStyle(
-                         color: Colors.blue[500],
-                         fontWeight: FontWeight.bold
-                       ),
-                       ),
-                     ],
+                      children: [
+                        Text(
+                          'Not a member?',style: TextStyle(
+                            color: Colors.grey[700]
+                        ),
+                        ),
+                        const SizedBox(width: 10,),
+                        Text(
+                          'Register now',style: TextStyle(
+                            color: Colors.blue[500],
+                            fontWeight: FontWeight.bold
+                        ),
+                        ),
+                      ],
                     ),
                   ),
                 )
